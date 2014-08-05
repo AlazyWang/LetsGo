86dac213d8198cb8d08c81e26ba79c6483d5cf4f
robert <robert@teacher.local>
2014-08-05 16:47:54 +0800

    CGAffineTransformMake

--- LetsGo/Home(首页)/Controller/IWHomeViewController.m
+++ LetsGo/Home(首页)/Controller/IWHomeViewController.m
@@ -17,9 +17,15 @@
 #import "LGFriendStatusesParam.h"
 #import "LGUserInfoParam.h"
 #import "LGUserTool.h"
+#import "MJRefresh.h"
+#import "UIImage+LG.h"
 
-@interface IWHomeViewController ()
-@property (nonatomic,strong)NSArray *arrayOfStatues;
+@interface IWHomeViewController () <MJRefreshBaseViewDelegate>
+@property (nonatomic,strong)NSMutableArray *arrayOfStatues;
+
+@property (nonatomic,strong)MJRefreshHeaderView *header;
+@property (nonatomic,strong)MJRefreshFooterView *footer;
+
 @end
 
 @implementation IWHomeViewController
@@ -27,9 +33,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.arrayOfStatues = [NSArray array];
+    self.arrayOfStatues = [NSMutableArray array];
     
     self.view.backgroundColor = LGglobalColor;
+    
+    [self setupRefrsh];
     
     // 1.导航条的按钮
 //    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImage:@"navigationbar_friendsearch" higlightedImage:@"navigationbar_friendsearch_highlighted" target:self action:@selector(addFriends)];
@@ -46,14 +54,14 @@
 //    } failure:^(NSError *error) {
 //        
 //    }];
-    LGFriendStatusesParam *result = [[LGFriendStatusesParam alloc]init];
-    result.access_token = [LGAccount currentAccount].access_token;
-    [LGStatusTool statusesWithAccessParam:result success:^(LGFriendStatusesResult *result) {
-        self.arrayOfStatues = result.statuses;
-        [self.tableView reloadData];
-    } failure:^(NSError *error) {
-        
-    }];
+//    LGFriendStatusesParam *result = [[LGFriendStatusesParam alloc]init];
+//    result.access_token = [LGAccount currentAccount].access_token;
+//    [LGStatusTool statusesWithAccessParam:result success:^(LGFriendStatusesResult *result) {
+//        self.arrayOfStatues = result.statuses;
+//        [self.tableView reloadData];
+//    } failure:^(NSError *error) {
+//        
+//    }];
     
     
     LGUserInfoParam *param = [[LGUserInfoParam alloc]init];
@@ -65,6 +73,125 @@
     } failure:nil];
     
      
+}
+
+
+- (void)setupRefrsh
+{
+    _header = [MJRefreshHeaderView header];
+    _header.delegate = self;
+    _header.scrollView = self.tableView;
+    [_header beginRefreshing];
+    
+    _footer = [MJRefreshFooterView footer];
+    _footer.delegate = self;
+    _footer.scrollView = self.tableView;
+    
+}
+
+- (void)dealloc
+{
+    [_footer free];
+    [_header free];
+}
+
+- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
+{
+    if ([refreshView isKindOfClass:[MJRefreshFooterView class]]) {
+        [self loadMoreStatues];
+    } else
+    {
+        [self loadNewStatues];
+    }
+}
+
+- (void)loadMoreStatues
+{
+    
+    LGFriendStatusesParam *param = [[LGFriendStatusesParam alloc]init];
+       if (_arrayOfStatues.count >0) {
+        LGStatus *firstStatu = [_arrayOfStatues lastObject];
+        
+        long long sinceId = [firstStatu.idstr longLongValue] -1;
+        param.max_id = @(sinceId);
+       }
+    param.access_token = [LGAccount currentAccount].access_token;
+    
+    [LGStatusTool statusesWithAccessParam:param success:^(LGFriendStatusesResult *result) {
+        [_arrayOfStatues addObjectsFromArray:result.statuses];
+        [self.tableView reloadData];
+        [_footer endRefreshing];
+        
+    } failure:^(NSError *error) {
+        [_footer endRefreshing];
+    }];
+  
+}
+
+
+- (void)loadNewStatues
+{
+    
+    LGFriendStatusesParam *param = [[LGFriendStatusesParam alloc]init];
+    if (_arrayOfStatues.count >0) {
+        LGStatus *firstStatu = _arrayOfStatues[0];
+        
+        long long sinceId = [firstStatu.idstr longLongValue];
+        param.since_id = @(sinceId);
+    }
+    param.access_token = [LGAccount currentAccount].access_token;
+    
+    [LGStatusTool statusesWithAccessParam:param success:^(LGFriendStatusesResult *result) {
+        NSMutableArray *newStatuses = [NSMutableArray array];
+        
+        [newStatuses addObjectsFromArray:result.statuses];
+        
+        [newStatuses addObjectsFromArray:_arrayOfStatues];
+        
+        _arrayOfStatues = newStatuses;
+        [self.tableView reloadData];
+        [_header endRefreshing];
+        
+        [self showNewStatusesNumber:result.statuses.count];
+        
+    } failure:^(NSError *error) {
+        [_header endRefreshing];
+    }];
+    
+    
+ 
+}
+
+- (void)showNewStatusesNumber:(int)number
+{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.userInteractionEnabled = NO;
+    [btn setBackgroundImage:[UIImage resizeWithName:@"timeline_new_status_background"] forState:UIControlStateNormal];
+    if (number == 0) {
+        [btn setTitle:[NSString stringWithFormat:@"没有微博"] forState:UIControlStateNormal];
+    } else{
+        [btn setTitle:[NSString stringWithFormat:@"%d条新微博", number] forState:UIControlStateNormal];
+    }
+    
+    CGFloat btnX = 0;
+    CGFloat btnW = self.view.frame.size.width;
+    CGFloat btnH = 30.0f;
+    CGFloat btnY = 64 - btnH;
+    
+    btn.frame = CGRectMake(btnX, btnY, btnW, btnH);
+    
+    [self.navigationController.view insertSubview:btn belowSubview:self.navigationController.navigationBar];
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        btn.transform = CGAffineTransformMakeTranslation(0, btnH);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1.0f delay:1.0f options:UIViewAnimationOptionLayoutSubviews animations:^{
+            btn.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            [btn removeFromSuperview];
+        }];
+    }];
+    
 }
 /**
  *  添加朋友
